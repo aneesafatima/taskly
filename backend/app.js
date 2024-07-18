00bab1cbe2d19676e130fc2edf6118e4feefd43e
aneesa04 <aneesafatima238@gmail.com>
2024-07-18 13:05:26 +0530

    connected with database

---
+++ backend/app.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const app = express(); //app is an instance of express
+app.use(express.json());
---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DB_CONNECTION_STRING.replace(
+  "<password>",
+  process.env.DB_PASSWORD
+);
+
+mongoose
+  .connect(DB, {})
+  .then(() => console.log("SUCCESSFULLY CONNECTED TO DATABASE"))
+  .catch((err) => console.log(err));
+
+console.log(process.env.NODE_ENV);
