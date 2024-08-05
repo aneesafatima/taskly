b9969d63d79fa056667f6ee15788ae3064cd1181
aneesa04 <aneesafatima238@gmail.com>
2024-08-05 13:20:06 +0530

    feat: Added a not found page

---
+++ frontend/src/components/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFound = ({link, code, location, message}) => {
+  return (
+    <div className='h-screen  m-auto text-3xl font-roboto text-medium text-gray-800 text-center'>
+      <h1>{code}</h1>
+      <p>{message}</p>
+      <span >Go to <Link to={link}><span className='text-blue-700 hover:underline active:underline'>{location}</span></Link></span>
+    </div>
+  );
+};
+
+export default NotFound;
