0373f605b564ae4172c087bc9ddc7cdb26d40e62
aneesa04 <aneesafatima238@gmail.com>
2024-08-10 18:18:51 +0530

    fixing bug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'frontend/dist' // This should match what you specify in `vercel.json`
+  }
 })
