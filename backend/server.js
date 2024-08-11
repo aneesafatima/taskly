f47cb10dcf1a9f31409f637695f756a688a03f8b
aneesa04 <aneesafatima238@gmail.com>
2024-08-11 17:43:13 +0530

    fixing deployment issues

--- backend/server.js
+++ backend/server.js
@@ -11,17 +11,21 @@ const DB = process.env.DB_CONNECTION_STRING.replace(
 mongoose
   .connect(DB, {})
   .then(() => console.log("SUCCESSFULLY CONNECTED TO DATABASE"))
-  .catch((err) => console.log(err));
+  .catch((err) => {
+    console.error("DATABASE CONNECTION ERROR:", err);
+    process.exit(1); //appication halting with an error
+  });
 
 console.log(process.env.NODE_ENV);
 
-
-module.exports = (req, res) => {
-  // Delegate request handling to the Express app
-  app(req, res); // This works because `app` is a function that Express set up to handle requests
-};
-
-// const server = app.listen(process.env.PORT, () => {
-//   //listening for requests to the specified port
-//   console.log("Listening...");
-// }); //this returns a server
+if (process.env.NODE_ENV === "development") {
+  const server = app.listen(process.env.PORT, () => {
+    //listening for requests to the specified port
+    console.log("Listening...");
+  }); //this returns a server
+} else {
+  module.exports = (req, res) => {
+    // Delegate request handling to the Express app
+    app(req, res); // This works because `app` is a function that Express set up to handle requests
+  };
+}
