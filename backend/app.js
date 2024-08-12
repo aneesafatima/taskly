083d4598d4c34ad043d5c801d38b96c27d67ca24
aneesa04 <aneesafatima238@gmail.com>
2024-08-12 20:20:54 +0530

    changed file structure

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+
+const DB = process.env.DB_CONNECTION_STRING.replace(
+  "<password>",
+  process.env.DB_PASSWORD
+);
 const userRouter = require("./routes/userRouter");
 const taskRouter = require("./routes/taskRouter");
 const dashboardRouter = require("./routes/dashboardRouter");
@@ -19,15 +27,30 @@ app.use(cookieParser());
 // Middleware to parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("api/users", userRouter);
-app.use("api/dashboard", dashboardRouter);
-app.use("api/tasks", taskRouter);
-app.use("api/settings", settingsRouter);
-app.use("*", (res,req) => {
+app.use("/api/users", userRouter);
+app.use("/api/dashboard", dashboardRouter);
+app.use("/api/tasks", taskRouter);
+app.use("/api/settings", settingsRouter);
+app.use("*", (req,res) => {
 res.status(404).json({
     message: "Not Found"
 })
 })
 app.use(errorController);
+
+mongoose
+  .connect(DB, {})
+  .then(() => console.log("SUCCESSFULLY CONNECTED TO DATABASE"))
+  .catch((err) => {
+    console.error("DATABASE CONNECTION ERROR:", err);
+    process.exit(1); //appication halting with an error
+  });
+  
+  if (process.env.NODE_ENV === "development") {
+    const server = app.listen(process.env.PORT, () => {
+      //listening for requests to the specified port
+      console.log("Listening...");
+    }); //this returns a server
+  }
 
 module.exports = app;
