b7351ef436bd5ae232e8a486e7a022eed8fa0e61
aneesa04 <aneesafatima238@gmail.com>
2024-08-12 21:09:14 +0530

    changing file structure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../public'  // Output directory at the root of your project
-  }
+  // build: {
+  //   outDir: '../public'  // Output directory at the root of your project
+  // }
 })
