08d5a809a5a96d075157d42b40449791218f47a0
aneesa04 <aneesafatima238@gmail.com>
2024-08-15 13:45:59 +0530

    trying to fix cors

--- backend/index.js
+++ backend/index.js
@@ -17,25 +17,25 @@ const app = express(); //app is an instance of express
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const settingsRouter = require("./routes/settingsRouter");
-// app.use(
-//   cors(
-//     {
-//       origin: [
-//         // "https://taskly-frontend-omega.vercel.app",
-//         "http://localhost:5173",
-//       ], // Your frontend origin
-//       // methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
-//       credentials: true,
-//     } // Allows credentials (cookies) to be sent
-//   )
-// );
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://taskly-frontend-omega.vercel.app'); // Your frontend origin
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); // If you need to send cookies
-  next();
-});
+app.use(
+  cors(
+    {
+      origin: [
+        "https://taskly-frontend-omega.vercel.app",
+        "http://localhost:5173",
+      ], // Your frontend origin
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+      credentials: true,
+    } // Allows credentials (cookies) to be sent
+  )
+);
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://taskly-frontend-omega.vercel.app'); // Your frontend origin
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true'); // If you need to send cookies
+//   next();
+// });
 app.use(express.json());
 app.use(cookieParser());
 // Middleware to parse application/x-www-form-urlencoded
